8a8c538aee39bc5eb0b913e1d0c586614c713d8e
Ibeenoch <94024099+Ibeenoch@users.noreply.github.com>
2023-02-21 02:50:18 +0100

    Update logistics.js
    Db

--- controller/logistics.js
+++ controller/logistics.js
@@ -3,10 +3,10 @@ import { io } from '../index.js'
 
 // Set up a MySQL connection 
 let pool = mysql.createConnection({
-  host: 'sql7.freemysqlhosting.net',
-  user: 'sql7599716',
-  password: '95f6glXBk9',
-  database: 'sql7599716',
+  host: 'sql.freedb.tech',
+  user: 'freedb_ibeenochcivo',
+  password: '?%&H7*ubBvNaN9a',
+  database: 'freedb_transportation',
   multipleStatements: true,
 });
 
